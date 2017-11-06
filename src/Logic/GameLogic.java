a620f46f4ba3563197b1609b5a1570e266005600
Matt Elbé <matt.leboucher@gmail.com>
2017-11-06 02:22:18 -0200

    Updates the interface for game logic systems.

--- src/Logic/GameLogic.java
+++ src/Logic/GameLogic.java
@@ -7,11 +7,27 @@ package Logic;
  */
 
 public interface GameLogic {
+    /**
+     * Perform the initialization tasks such a preparing the memory or loading game-specific data.
+     *
+     * @throws Exception
+     */
     void initialize() throws Exception;
 
+    /**
+     * Retrieves and processes the inputs.
+     */
     void processInput();
 
+    /**
+     * Wraps the whole game logic per se and updates the game state.
+     *
+     * @param timeInterval The time step after which the game logic should be updated.
+     */
     void update(float timeInterval);
 
+    /**
+     * Renders the logic objects and game-specific data.
+     */
     void render();
 }
