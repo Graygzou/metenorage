704e153c09d8b0e87d303d38f828badd42a418ae
matt.leboucher <matt.leboucher@gmail.com>
2017-11-07 21:27:43 -0500

    Fixes #11.

---
+++ src/Helper/Timer.java
@@ -0,0 +1,36 @@
+package Helper;
+
+public class Timer {
+    /*
+     * The instant at which last loop occurred.
+     */
+    private double lastLoopTime;
+
+    /**
+     * @return The current system time.
+     */
+    public double getTime() {
+        return System.nanoTime() / 1000000000.0;
+    }
+
+    /**
+     * Computes the time since last loop.
+     *
+     * @return The elapsed time since last loop occurrence.
+     */
+    public float getElapsedTime() {
+        double time = this.getTime();
+        float elapsedTime = (float) (time - lastLoopTime);
+        lastLoopTime = time;
+
+        return elapsedTime;
+    }
+
+    public double getLastLoopTime() {
+        return lastLoopTime;
+    }
+
+    public void initialize() {
+        lastLoopTime = this.getTime();
+    }
+}
