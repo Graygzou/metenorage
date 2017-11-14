c60cf725d6fc8a3c213bd54056b751e1bee6d4d2
matt.leboucher <matt.leboucher@gmail.com>
2017-11-14 14:27:31 -0500

    Fixes #17.

---
+++ src/Engine/Utils.java
@@ -0,0 +1,14 @@
+package Engine;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+/**
+ * @author : Matthieu Le Boucher
+ */
+public class Utils {
+    public static String readTextResource(String filename) throws IOException {
+        return new String(Files.readAllBytes(Paths.get("../resources/" + filename)));
+    }
+}
