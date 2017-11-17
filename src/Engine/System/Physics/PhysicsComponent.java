415093fdfe97748731d0072d8221cdf5e4b6ee0c
matt.leboucher <matt.leboucher@gmail.com>
2017-11-16 19:44:01 -0500

    Refactoring: creates Maven build.

--- src/Engine/System/Physics/PhysicsComponent.java
+++
@@ -1,9 +0,0 @@
-package Engine.System.Physics;
-
-import Engine.System.Component.Component;
-
-/**
- * @author Matthieu Le Boucher <matt.leboucher@gmail.com>
- */
-public interface PhysicsComponent extends Component {
-}
