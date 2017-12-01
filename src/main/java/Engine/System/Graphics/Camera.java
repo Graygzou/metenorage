b517eb25c85b83d823612456c090982843c9c025
Matt Elbé <matt.leboucher@gmail.com>
2017-11-30 22:00:18 -0500

    Camera is now an entity.

--- src/main/java/Engine/System/Graphics/Camera.java
+++ src/main/java/Engine/System/Graphics/Camera.java
@@ -1,16 +1,12 @@
 package Engine.System.Graphics;
 
+import Engine.Main.Entity;
 import org.joml.Vector3f;
 
 /**
  * @author : Matthieu Le Boucher <matt.leboucher@gmail.com>
  */
-public class Camera {
-
-    private final Vector3f position;
-
-    private final Vector3f rotation;
-
+public class Camera extends Entity {
     public Camera() {
         position = new Vector3f(0, 0, 0);
         rotation = new Vector3f(0, 0, 0);
@@ -19,43 +15,5 @@ public class Camera {
     public Camera(Vector3f position, Vector3f rotation) {
         this.position = position;
         this.rotation = rotation;
-    }
-
-    public Vector3f getPosition() {
-        return position;
-    }
-
-    public void setPosition(float x, float y, float z) {
-        position.x = x;
-        position.y = y;
-        position.z = z;
-    }
-
-    public void movePosition(float offsetX, float offsetY, float offsetZ) {
-        if (offsetZ != 0) {
-            position.x += (float) Math.sin(Math.toRadians(rotation.y)) * -1.0f * offsetZ;
-            position.z += (float) Math.cos(Math.toRadians(rotation.y)) * offsetZ;
-        }
-        if (offsetX != 0) {
-            position.x += (float) Math.sin(Math.toRadians(rotation.y - 90)) * -1.0f * offsetX;
-            position.z += (float) Math.cos(Math.toRadians(rotation.y - 90)) * offsetX;
-        }
-        position.y += offsetY;
-    }
-
-    public Vector3f getRotation() {
-        return rotation;
-    }
-
-    public void setRotation(float x, float y, float z) {
-        rotation.x = x;
-        rotation.y = y;
-        rotation.z = z;
-    }
-
-    public void moveRotation(float offsetX, float offsetY, float offsetZ) {
-        rotation.x += offsetX;
-        rotation.y += offsetY;
-        rotation.z += offsetZ;
     }
 }
