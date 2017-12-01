8d9ad01ac1acfa6801694632089df2980acf135c
Matt Elbé <matt.leboucher@gmail.com>
2017-12-01 00:31:23 -0500

    Generates a grid of cubes.

--- src/main/java/Game/Minedraft.java
+++ src/main/java/Game/Minedraft.java
@@ -10,30 +10,23 @@ import Engine.System.Graphics.Camera;
 import Engine.System.Graphics.Component.Cube;
 import Game.Input.CameraFollow;
 
+import java.util.Random;
+
 public class Minedraft {
     public static void main(String[] args) {
         try {
             GameEngine gameEngine = new GameEngine("Minedraft", 800, 600);
 
-            Entity block1 = new Entity("Block 1");
-            Entity block2 = new Entity("Block 2");
-            Entity block3 = new Entity("Block 3");
-            block1.addComponent(new Cube(block1, "/Game/Textures/grassblock.png"));
-            block2.addComponent(new Cube(block2, "/Game/Textures/grassblock.png"));
-            block3.addComponent(new Cube(block3, "/Game/Textures/grassblock.png"));
+            int gridWidth = 10, gridHeight = 10;
 
-
-            block1.setPosition(0, 0, -2f);
-            block1.setScale(1f);
-            block2.setPosition(1f, 1f, -2f);
-            block2.setScale(1f);
-            block3.setPosition(-1f, 0f, -2f);
-            block3.setScale(1f);
-
-            gameEngine.addEntity(block1);
-            gameEngine.addEntity(block2);
-            gameEngine.addEntity(block3);
-
+            for (float i = 0; i < gridWidth; i++) {
+                for (float j = 0; j < gridHeight; j++) {
+                    Entity block = new Entity("Block (" + i + ", " + j + ")");
+                    block.addComponent(new Cube(block, "/Game/Textures/grassblock.png"));
+                    block.setPosition(i, (new Random()).nextFloat() * 3, -2f -j);
+                    gameEngine.addEntity(block);
+                }
+            }
 
             Camera mainCamera = new Camera();
             mainCamera.setName("Caméra principale");
