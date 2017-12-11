16cd164f7b2dd2896c7a06a33c1d7b2735262254
Matt Elbé <matt.leboucher@gmail.com>
2017-12-10 20:59:46 -0500

    Creates a textual HUD.

---
+++ src/main/java/Engine/Main/TextualHUD.java
@@ -0,0 +1,41 @@
+package Engine.Main;
+
+/*
+ * @author Matthieu Le Boucher <matt.leboucher@gmail.com>
+ */
+
+import Engine.System.Graphics.Component.Text2D;
+import Engine.System.Graphics.HUD;
+import Engine.Window;
+import org.joml.Vector4f;
+
+public class TextualHUD implements HUD {
+    private static int FONT_COLUMNS = 8;
+
+    private static int FONT_ROWS = 8;
+
+    private static final String FONT_TEXTURE = "/Game/Fonts/Segoe.png";
+
+    private final Text2D[] textMeshes;
+
+    private final Text2D statusText;
+
+    public TextualHUD(String characters) throws Exception {
+        this.statusText = new Text2D(new Entity(), characters, FONT_TEXTURE, FONT_COLUMNS, FONT_ROWS);
+        this.statusText.getMaterial().setAmbientColor(new Vector4f(1, 1, 1, 1));
+        textMeshes = new Text2D[]{statusText};
+    }
+
+    public void setStatusText(String statusText) {
+        this.statusText.setText(statusText);
+    }
+
+    public void updateSize(Window window) {
+        this.statusText.getEntity().setPosition(10f, window.getHeight() - 50f, 0);
+    }
+
+    @Override
+    public Text2D[] getHUDMeshes() {
+        return textMeshes;
+    }
+}
